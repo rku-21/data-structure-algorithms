32ea8c859ac2c0d3c2a8ca8d3171cbbd18e112bc
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-08-11 16:39:47 +0530

    Time: 1381 ms (5.01%), Space: 28.1 MB (45.1%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -33,7 +33,7 @@ public:
     bool isMatch(string s, string p) {
         m=s.size();
         n=p.size();
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         unordered_map<int,int>shrink_stars;
         int start=-1;
         int i=0;
@@ -48,7 +48,61 @@ public:
         }
        
 
-        return solve(0,0,s,p,shrink_stars);
+        // return solve(0,0,s,p,shrink_stars);
+
+        // Tabulation dp 
+
+        dp[m][n]=true;
+
+        for(int i=0; i<m; i++) dp[i][n]=false;
+        for(int j=0; j<n; j++){
+            if(p[j]=='*' && shrink_stars[j]==n) dp[m][j]=true;
+            else dp[m][j]=false;
+        }
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(p[j]>='a' && p[j]<='z'){
+                    if(s[i]!=p[j]){
+                        dp[i][j]=false;
+                    }
+                    else dp[i][j]=dp[i+1][j+1];
+
+                }
+                else if(p[j]=='?'){
+                    dp[i][j]=dp[i+1][j+1];
+                }
+                else {
+                    bool skip=dp[i][shrink_stars[j]];
+                    bool match_curr=dp[i+1][j];
+
+                    dp[i][j]= skip || match_curr;
+                }
+            }
+        }
+
+        return dp[0][0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 };
