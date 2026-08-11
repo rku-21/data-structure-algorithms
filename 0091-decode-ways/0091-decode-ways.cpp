5d3446055350311d51edbd160f9dca1283f27eb5
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-08-11 15:58:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.28%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -27,8 +27,33 @@ public:
     int numDecodings(string s) {
          n=s.size();
         if(s[0]=='0') return 0;
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s);
+        memset(dp,0,sizeof(dp));
+        // return solve(0,s);
+
+        dp[n]=1;
+
+        for(int i=n-1; i>=0; i--){
+            int ans=0;
+            int digit1=s[i]-'0';
+
+            if(digit1){
+                ans+=dp[i+1];
+
+                if(i+1<n){
+                   int  digit2=s[i+1]-'0';
+                    int num=digit1*10+digit2;
+                    if(num<=26){
+                        ans+=dp[i+2];
+                    }
+                }
+            }
+            dp[i]=ans;
+        }
+
+        return dp[0];
+
+
+
 
         
     }
