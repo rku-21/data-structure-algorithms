fe81fe316c3d9c3f6534e46fe0d74a5bac0864d4
Rahul Kumar Upadhyay <rahul.k.upadhyay21@gmail.com>
2026-09-15 19:06:24 +0530

    Time: 391 ms (15.44%), Space: 36.9 MB (24.08%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int n;
-    bool ispalin(int i, int j, auto&s){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
-        } 
-        return true;
-    }
+   
     int dp[2001][2001];
-    int solve(int i, int j, auto&s, int k){
+    int solve(int i, int j, auto&s, int k,auto&isPalin){
         if(i>=n || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
 
         int ans=0;
 
-        bool ok=ispalin(i, j,s);
+        bool ok=isPalin[i][j];
 
         if(ok){
-            ans=1+solve(j+1,j+k, s, k);
+            ans=1+solve(j+1,j+k, s, k, isPalin);
         }
 
         else {
-            ans=max(solve(i+1,j+1,s,k), solve(i,j+1,s,k));
+            ans=max(solve(i+1,j+1,s,k,isPalin), solve(i,j+1,s,k, isPalin));
         }
 
         return dp[i][j]=ans;
@@ -36,8 +29,25 @@ public:
     int maxPalindromes(string s, int k) {
         n=s.size();
         memset(dp, -1,sizeof(dp));
+        vector<vector<bool>>isPalin(n, vector<bool>(n,false));
 
-        return solve(0, k-1,s, k);
+        for(int len=1; len<=n; len++){
+            for(int i=0; i<=n-len; i++){
+                int j=i+len-1;
+
+                if(len == 1){
+                    isPalin[i][j]=true;
+                }
+                else if(len==2){
+                    isPalin[i][j]= s[i]==s[j];
+                }
+                else {
+                    isPalin[i][j]= s[i]==s[j] && isPalin[i+1][j-1];
+                }
+            }
+        }
+
+        return solve(0, k-1,s, k,isPalin);
         
     }
 };
